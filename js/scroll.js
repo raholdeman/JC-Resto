22b482704a2ec9c9bb547d77ef11d2f737a94f5f
Ryan <antonholdeman@gmail.com>
2016-02-02 08:49:26 -0700

    adding basic code
    added my html and css files to the repository

---
+++ js/scroll.js
@@ -0,0 +1,45 @@
+var currentIndex = 0,
+	items = $('.container div'),
+	itemAmt = items.length;
+
+function cycleItems() {
+	var item = $('.container div').eq(currentIndex);
+	items.hide();
+	item.css('display','inline-block');
+}
+
+var autoSlide = setInterval(function() {
+	currentIndex += 1;
+	if (currentIndex > itemAmt - 1) {
+		currentIndex = 0;
+	}
+	cycleItems();
+}, 3000);
+
+$('.next').click(function() {
+	clearInterval(autoSlide);
+	currentIndex += 1;
+	if (currentIndex > itemAmt - 1) {
+		currentIndex = 0;
+	}
+	cycleItems();
+});
+
+$('.prev').click(function() {
+	clearInterval(autoSlide);
+	currentIndex -= 1;
+	if (currentIndex < 0) {
+		currentIndex = itemAmt - 1;
+	}
+	cycleItems();
+});
+
+
+/**************************************Menu*****************************************/
+
+$('.menu2','menu3','menu4','menu5').hide();
+
+/*$('.brkFst').click(function(){
+	$('.menu1').show();
+});
+*/
